361ee0a279086645bdf7e931fb63df42e8178810
Meulemans Philippe <90241977+Laverdure77@users.noreply.github.com>
2023-01-09 11:02:25 +0100

    add gitignore

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+#_____________________________________________________________________________
+
+from .scripts.colors import bcolors as bc
+#_____________________________________________________________________________
+print (f"{bc.HEADER}Hello{bc.ENDC}")

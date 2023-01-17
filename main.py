121e210757677b65e5411bd0be640fe5d657e335
Meulemans Philippe <90241977+Laverdure77@users.noreply.github.com>
2023-01-17 18:37:41 +0100

    remove main.py from repo

--- main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-#_____________________________________________________________________________
-
-from scripts.colors import bcolors as bc
-#_____________________________________________________________________________
-file = './datas/Immoweb_data_ok_maite.csv'
-data = pd.read_csv(file)
-data.head()

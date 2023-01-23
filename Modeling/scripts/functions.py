e1701b1e25a7debeb407e28092ad7ec9249bc65e
Meulemans Philippe <meulemansph@hotm.com>
2023-01-23 17:03:36 +0100

    end of day

--- Modeling/scripts/functions.py
+++ Modeling/scripts/functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Create and prepare dataset from data scrapping csv
 def get_dataset(_csv_path: str) -> pd.DataFrame :
@@ -16,8 +17,11 @@ def get_dataset(_csv_path: str) -> pd.DataFrame :
     _data = _data.drop(_data[_data['House or appartment?'] == 'HOUSE_GROUP'].index)
     # Remove Public sales
     _data = _data.drop(_data[_data['Type of sale'] == 'PUBLIC_SALE'].index)
+    _data['Number of rooms'] = _data['Number of rooms'].astype(int)
+    _data['Number of facades'] = _data['Number of facades'].astype(int)
     # Reset index after drop
     # _data = _data.reset_index()
+
     return _data
 
 # Add zipcode columns from csv, based on locality column
@@ -58,8 +62,38 @@ def add_province(_data: pd.DataFrame) -> pd.DataFrame :
     # _data = _data.reset_index()
     return _data
 
+# Add Region column based on zipcode column
+def add_region(_data: pd.DataFrame) -> pd.DataFrame :
+    _data.loc[_data['zip_code'].between(1000, 1299,'both'), 'Region'] = 'Brux'
+    _data.loc[_data['zip_code'].between(1300, 1499,'both'), 'Region'] = 'Wallonie'
+    _data.loc[_data['zip_code'].between(2000, 2999,'both'), 'Region'] = 'Flandre'
+    _data.loc[_data['zip_code'].between(3500, 3999,'both'), 'Region'] = 'Flandre'
+    _data.loc[_data['zip_code'].between(4000, 4999,'both'), 'Region'] = 'Wallonie'
+    _data.loc[_data['zip_code'].between(5000, 5680,'both'), 'Region'] = 'Wallonie'
+    _data.loc[_data['zip_code'].between(6600, 6999,'both'), 'Region'] = 'Wallonie'
+    _data.loc[_data['zip_code'].between(8000, 8999,'both'), 'Region'] = 'Flandre'
+    _data.loc[_data['zip_code'].between(9000, 9999,'both'), 'Region'] = 'Flandre'
+    _data.loc[_data['zip_code'].between(1500, 1999,'both'), 'Region'] = 'Flandre'
+    _data.loc[_data['zip_code'].between(3000, 3499,'both'), 'Region'] = 'Flandre'
+    _data.loc[_data['zip_code'].between(6000, 6599,'both'), 'Region'] = 'Wallonie'
+    _data.loc[_data['zip_code'].between(7000, 7999,'both'), 'Region'] = 'Wallonie'
+    # Remove rows where PRegion is missing
+    _data = _data.drop((_data[_data['Region'].isna()].index))
+    # Reset index after drop
+    # _data = _data.reset_index()
+    return _data
+
+# Slice df to target and features
 def set_feat_target(_data: pd.DataFrame) -> np.ndarray:
-    # Set traget and features
+    # Set target and features
     _y = _data[['Price']].to_numpy()
     _X = _data.drop(columns='Price').to_numpy()
     return _X, _y
+
+# Plot regression line from intercept and coef
+def regline(_slope: float, _intercept: float, _color: str ):
+    """Plot a line from slope and intercept"""
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = _intercept + _slope * x_vals
+    plt.plot(x_vals, y_vals, '--', color=_color)
